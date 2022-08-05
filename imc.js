4c042bc92dbeb9fb1c737884fa47e06acb1105a4
pedro8811 <pedrouniessa881@gmail.com>
2022-08-05 09:19:04 -0300

    0.1.0

--- imc.js
+++ imc.js
@@ -1,6 +1,5 @@
 const calcular = document.getElementById('calcular');
 
-
 function imc() {
     const nome = document.getElementById('nome').value;
     const altura = document.getElementById('altura').value;
